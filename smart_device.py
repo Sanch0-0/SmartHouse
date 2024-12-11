fc3feee0085b207d00ee79fdf75d7d3a6d5c53dd
Sanch0-0 <fff.alex.er.fff@gmail.com>
2024-12-11 21:16:10 +0600

    SmartDevice class has been writen

--- smart_device.py
+++ smart_device.py
@@ -29,21 +29,46 @@ class SmartDevice:
         self.device_name = device_name
         self.brand = brand
         self.power_consumption = power_consumption
-        self.network_connection = network_connection
+        self.network_connection = network_connection.capitalize()
         self._status = "Off"
-        self._location = None # Положение в комнате
+        self._location = None
+        self._floor = None
 
-    def validate_data():
-        pass
+    def validate_data(self):
+        appropriate_connections = ["Wi-Fi", "Bluetooth", "Ethernet"]
 
-    def turn_on():
-        pass
+        if self.power_consumption >= 220:
+            raise ValueError(f"Device '{self.device_name}' is too energy-consuming!")
+        elif not self.power_consumption.isdigit():
+            raise ValueError("power_consuption must be an integer!")
+        
+        if self.network_connection not in appropriate_connections:
+            raise ValueError("'network_connection' must be Wi-Fi / Bluetooth / Ethernet")
 
-    def turn_off():
-        pass
+    def turn_on(self):
+        self._status = "On"
+        print("Device has been enabled.")
 
-    def change_location():
-        pass
+    def turn_off(self):
+        self._status = "Off"
+        print("Device has been disabled.")
 
-    def perform_action():
-        pass
+    def set_location(self, room, floor):
+        if room.isalpha() and floor.isdigit():
+            self._location = room
+
+            if floor == 1:
+                self._floor = floor + "st"
+            elif floor == 2:
+                self._floor = floor + "nd"
+            elif floor == 3:
+                self._floor = floor + "rd"
+            else:
+                self._floor = floor + "th"
+        else:
+            raise ValueError("Invalid arguments' type!")
+
+        print(f"New location for {self.device_name}: {self._location} on {self._floor} floor.")
+
+    def perform_action(self):
+        print("Device is doing it's job...")
